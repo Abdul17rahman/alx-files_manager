248c9758d8a840be771702e498fc3f804ec2daf7
Abdul Rahman <abdulnsamba@gmail.com>
2024-06-17 21:32:22 +0300

    redis

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+const { createClient } = require('redis')
+
+class RedisClient {
+    constructor() {
+        this._client = createClient()
+        this._client.on('error', err => {
+            console.log(err)
+        })
+    }
+    isAlive() {
+        this._client.on('success', () => {
+            return true
+        })
+        return false
+    }
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this._client.get(key, (err, data) => {
+                if (err) reject(err)
+                resolve(data)
+            })
+        })
+    }
+    async set(key, val, dur) {
+        return new Promise((resolve, reject) => {
+            this._client.setex(key, dur, val, (err, reply) => {
+                if (err) reject(err)
+                resolve(reply)
+            })
+        })
+    }
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this._client.del(key, (err) => {
+                if (err) reject(err)
+                resolve()
+            })
+        })
+    }
+}
+
+const redisClient = new RedisClient()
+
+module.exports = redisClient
+
