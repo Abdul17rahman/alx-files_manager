34a147e5a34ec9ad093eab63f7ddd3a569e47cc3
Abdul Rahman <abdulnsamba@gmail.com>
2024-06-18 18:25:47 +0300

    fixl

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,23 +1,22 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AppController {
-	static getStatus(req, res) {
-  		if (redisClient.isAlive() && dbClient.isAlive()) {
-    		res.status(200).send({
-      			redis: true,
-      			db: true,
-    		});
-  		}
-	}
-	static async getStats(req, res) {
-  		const users = await dbClient.nbFiles();
-  		const files = await dbClient.nbUsers();
-  		return res.status(200).send({
-    		users,
-    		files,
-  		});
-	}
+  static getStatus(req, res) {
+    const status = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.send(status);
+  }
+
+  static async getStats(req, res) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.send(stats);
+  }
 }
 
 module.exports = AppController;
