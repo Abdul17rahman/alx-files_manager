ddad7cec36c7d8370b0ddcb6f50dd5afc0a8459e
Abdul Rahman <abdulnsamba@gmail.com>
2024-06-18 14:31:36 +0300

    fix db

--- utils/db.js
+++ utils/db.js
@@ -1,33 +1,30 @@
-const { MongoClient } = require('mongodb');
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'file_manager';
-    this.url = `mongodb://${this.host}:${this.port}/${this.database}`;
-    this._dbClient = new MongoClient(this.url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    this._dbClient.connect()
+    const port = process.env.DB_PORT || 27017;
+    const host = process.env.DB_HOST || 'localhost';
+    const dbName = process.env.DB_NAME || 'files_manager';
+    const url = `mongodb://${host}:${port}/${dbName}`;
+    this.client = new MongoClient(url);
+    this.client.connect();
   }
 
   isAlive() {
-    return this._dbClient.isConnected()
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    // returns number of documents in user collection.
-    return this._dbClient.db().collection('users').countDocuments();
+    const collection = this.client.db().collection('users');
+    return collection.countDocuments();
   }
 
   async nbFiles() {
-    // returns the number of documents in file collection
-    return this._dbClient.db().collection('files').countDocuments();
+    const collection = this.client.db().collection('files');
+    return collection.countDocuments();
   }
 }
 
 const dbClient = new DBClient();
 
-module.exports = dbClient;
+export default dbClient;
