0c8baf1e3825e07f1143d15ff0daf5d810f31cb4
Abdul Rahman <abdulnsamba@gmail.com>
2024-06-18 17:59:11 +0300

    fixx1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const AppRouter = require('./routes/index');
 
 const port = process.env.PORT || 5000;
 
-app.use(AppRouter);
+app.use('/', AppRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
